1651d29d618b3e823368b6a3f87e9062ecaaf135
shihominorth <shihomi0104@icloud.com>
2021-08-29 23:19:41 +0900

    add pod Action, enable to go to reset page

---
+++ TastyBox-2/Scene/LoginScene.swift
@@ -0,0 +1,8 @@
+//
+//  LoginScene.swift
+//  TastyBox-2
+//
+//  Created by 北島　志帆美 on 2021-08-29.
+//
+
+import Foundation
---
+++ TastyBox-2/Scene/SceneCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  SceneCoordinator.swift
+//  TastyBox-2
+//
+//  Created by 北島　志帆美 on 2021-08-29.
+//
+
+import Foundation
