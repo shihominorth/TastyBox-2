c9c7bcc63b4d90dec92f7949a7d2066491370919
Shihomi Kitajima <shihomi0104@icloud.com>
2021-11-11 16:15:48 +0900

    create following and followed users page

---
+++ TastyBox-2/ViewController/Profile/FollowingUsersViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FollowingUsersViewController.swift
+//  TastyBox-2
+//
+//  Created by 北島　志帆美 on 2021-11-11.
+//
+
+import UIKit
+
+class FollowingUsersViewController: UIViewController {
+    
+    // table view cell should be xib file or building progmatically.
+    // cuz the views in the cell is different depending on my following user or other's
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
